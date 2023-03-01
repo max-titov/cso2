498ba312324aa173d6e837946b67863d68b9d7e2
themaxt <max.teetov@gmail.com>
2023-03-01 12:42:20 -0500

    yay pagetable

--- hw-pagetable/main.c
+++ hw-pagetable/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 #include "mlpt.h"
 #include "config.h"
 
-size_t ptbr = NULL;
+
+size_t ptbr = 0;
+size_t* ptbr_pointer;
 size_t byte_to_bits = 8;
 size_t pte_size=8; //page table entry
 size_t pte_size_bits;
@@ -13,9 +16,11 @@ size_t vpn_size_bits; //virtual page number
 //size_t va_size; //virtual address
 size_t va_size_bits; //virtual address
 
-void set_constants(){
-	//SET PBTR
+size_t offset_mask(){
+	return pow(2,(POBITS))-1;
+}
 
+void set_constants(){
 	pte_size_bits=pte_size*byte_to_bits;
 
 	double vpn_size_d = log2(pow(2,POBITS)/pte_size)*LEVELS;
@@ -23,41 +28,77 @@ void set_constants(){
 	//vpn_size_bits=vpn_size*byte_to_bits;
 	//va_size=vpn_size +POBITS/byte_to_bits;
 	va_size_bits=vpn_size_bits+POBITS;
-	printf("vpn_size_bits: %zu, va_size_bits: %zu\n",vpn_size_bits,va_size_bits);
+	//printf("vpn_size_bits: %zu, va_size_bits: %zu\n",vpn_size_bits,va_size_bits);
 }
 
 void page_allocate(size_t va)
 {
-	if(ptbr == NULL){
+	if(ptbr == 0){
 		set_constants();
+
+		//SET PBTR
+		posix_memalign((void **)&ptbr, 1<<POBITS, 1<<POBITS);
+		ptbr_pointer = (size_t*)ptbr;
+		memset(ptbr_pointer, 0, 1<<POBITS); // fill with zeros
 	}
-	return;
+
+	size_t vpn_size_2 = POBITS -3;
+
+	size_t vpn = ((va>>POBITS) >> (vpn_size_2* (LEVELS - 1))) & ((1<<vpn_size_2)-1);
+
+	size_t *pte = ptbr_pointer+vpn;
+
+	if((*pte & 1) == 0){
+		//not valid
+		size_t new_pointer;
+		posix_memalign((void **)&new_pointer, 1<<POBITS, 1<<POBITS);
+		memset(new_pointer, 0, 1<<POBITS); // fill with zeros
+
+		*pte = new_pointer+1;
+
+		//ptbr_pointer =(size_t *)new_pointer;
+	}
 }
 
 size_t translate(size_t va)
 {
-	set_constants();
-	
-	size_t vpn = (va>>POBITS);
-	size_t offset = (va&0xFFF); // change to be based on POBITS
-	printf("vpn: %zx, offset: %zx\n",vpn,offset);
-	//treat each page table as an array and the vpn as the index into that array
-	size_t pte = 0; 
-	for(int i = 0; i<pte_size_bits; i+=1){ //there probably is a more efficient way to do this
-		size_t to_index = vpn*pte_size_bits+i;
-		size_t* ptbr_pointer = (size_t*)ptbr;
-		printf("ptbr_pointer: %zx, pte: %zx\n",ptbr_pointer,pte);
-		pte = (pte<<1)+(ptbr_pointer[to_index]);
+	if(ptbr == 0){
+		return -1;
 	}
-	if(!(pte & 1)){ //not valid
-		return NULL; // change to all 1s
+
+	size_t vpn_size_2 = POBITS-3;
+
+	size_t vpn = ((va>>POBITS) >> (vpn_size_2* (LEVELS - 1))) & ((1<<vpn_size_2)-1);
+
+	size_t *pte = ptbr_pointer+vpn;
+
+	if((*pte & 1) == 0){
+		return -1;
 	}
-	size_t ppn = pte>>POBITS; //remove the offset bits from pte to get ppn
 
-	size_t physical_addr = (ppn<<POBITS)+offset; // move ppn by offset bits and add offset bits from va
-
-	return physical_addr;
+	size_t ppn = (*pte & ~offset_mask());
+	size_t offset = (va&offset_mask());
+	return ppn + offset;
 }
+
+// size_t translate(size_t va)
+// {
+// 	if(ptbr == 0){
+// 		set_constants();
+// 	}
+	
+// 	size_t vpn = (va>>POBITS);
+// 	size_t offset = (va&offset_mask());
+	
+// 	size_t pte = ptbr_pointer[vpn]; 
+// 	if(!(pte & 1)){ //not valid
+// 		return ~0; //all 1s
+// 	}
+
+// 	size_t physical_addr = pte+offset-1; //
+
+// 	return physical_addr;
+// }
 
 // int main()
 // {	
--- lab-networking/netlab/netlab.c
+++ lab-networking/netlab/netlab.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 #include "netsim.h"
 
+char checksum(size_t data_len, void *data){
+	char checksum = 0;
+	for(int i = 1; i < data_len; i++){
+		checksum = checksum^((char *)data)[i];
+	}
+	return checksum;
+}
+
+void acknowledge (int seq_num){
+	char ack[5];
+    ack[1] = 'A'; ack[2] = 'C'; ack[3] = 'K'; ack[4] = seq_num+1;
+	ack[0]=checksum(5,ack);
+	send(5, ack);
+}
+
+void pnum(void *num) {
+    printf("%d\n", (int)num);
+}
+
 void recvd(size_t len, void* _data) {
     // FIX ME -- add proper handling of messages
     char *data = _data;
     fwrite(data+3,1,len,stdout);
     fflush(stdout);
+
+	acknowledge(data[4]);
+
+	int one = setTimeout(pnum, 1000, (void *)1uL);
+
+	clearTimeout(one);
+
+	//waitForAllTimeouts();
+	
+	
 }
 
 int main(int argc, char *argv[]) {
@@ -18,10 +47,9 @@ int main(int argc, char *argv[]) {
     data[1] = 'G'; data[2] = 'E'; data[3] = 'T'; data[4] = argv[1][0];
     // end of working code
     
-    
-    data[0] = 102; // FIX ME -- add proper checksum computation
-    send(5, data);
+    data[0]=checksum(5,data);
+	send(5, data);
     // FIX ME -- add action if no reply
-
+	
     waitForAllTimeouts();
 }
